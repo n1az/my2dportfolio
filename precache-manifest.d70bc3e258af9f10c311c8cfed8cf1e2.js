ce3e8420451336a24f8611e46c8941597f8a662b
Md Niaz Morshed <muhammadniazmorshed@gmail.com>
2024-01-29 21:40:54 +0100

    Updates

---
+++ precache-manifest.d70bc3e258af9f10c311c8cfed8cf1e2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cb38e977db4080f2b17d1caba9e4246",
+    "url": "/my2dportfolio/index.html"
+  },
+  {
+    "revision": "88b41dc7be6eb95172ac",
+    "url": "/my2dportfolio/static/css/main.d93ff38b.chunk.css"
+  },
+  {
+    "revision": "5c73db3cbd32cad6449d",
+    "url": "/my2dportfolio/static/js/2.8658e905.chunk.js"
+  },
+  {
+    "revision": "88b41dc7be6eb95172ac",
+    "url": "/my2dportfolio/static/js/main.1c97168b.chunk.js"
+  },
+  {
+    "revision": "3c39f2f932b67f136ebb",
+    "url": "/my2dportfolio/static/js/runtime~main.d48daa0d.js"
+  },
+  {
+    "revision": "87b552be888d1e13b0f31972ab354556",
+    "url": "/my2dportfolio/static/media/cv-x.87b552be.pdf"
+  },
+  {
+    "revision": "bae2ba18751a72ec8616d503575acdc4",
+    "url": "/my2dportfolio/static/media/levi.bae2ba18.png"
+  },
+  {
+    "revision": "831ddfe09140c8b580a9ce68dac5fcf3",
+    "url": "/my2dportfolio/static/media/min.831ddfe0.jpg"
+  }
+]);
